3a7f05d52ebdb2a3381d9ad87460934452e70c2f
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-03-11 11:01:07 +0000

    Added back other chains.

--- dapps/react-dapp-v2/src/constants/default.ts
+++ dapps/react-dapp-v2/src/constants/default.ts
@@ -1,24 +1,24 @@
 export const DEFAULT_MAIN_CHAINS = [
   // mainnets
-  // "eip155:1",
-  // "eip155:10",
-  // "eip155:100",
-  // "eip155:137",
-  // "eip155:42161",
-  // "eip155:42220",
-  // "cosmos:cosmoshub-4",
   "near:mainnet",
+  "eip155:1",
+  "eip155:10",
+  "eip155:100",
+  "eip155:137",
+  "eip155:42161",
+  "eip155:42220",
+  "cosmos:cosmoshub-4",
 ];
 
 export const DEFAULT_TEST_CHAINS = [
   // testnets
-  // "eip155:42",
-  // "eip155:69",
-  // "eip155:80001",
-  // "eip155:421611",
-  // "eip155:44787",
   "near:testnet",
   "near:betanet",
+  "eip155:42",
+  "eip155:69",
+  "eip155:80001",
+  "eip155:421611",
+  "eip155:44787",
 ];
 
 export const DEFAULT_CHAINS = [...DEFAULT_MAIN_CHAINS, ...DEFAULT_TEST_CHAINS];
