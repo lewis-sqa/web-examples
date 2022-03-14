ab0b5a677ced85f12d653a3a51578107fdcc23aa
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-03-14 11:29:02 +0000

    Initial attempt to generate NEAR accounts from mnemonic.

--- wallets/react-wallet-v2/src/utils/NearWalletUtil.ts
+++ wallets/react-wallet-v2/src/utils/NearWalletUtil.ts
@@ -1,18 +1,41 @@
-import { WalletConnection, connect, keyStores, transactions } from "near-api-js";
+import { WalletConnection, connect, keyStores, transactions, utils } from "near-api-js";
+import { parseSeedPhrase } from "near-seed-phrase";
+import { decode } from "bs58";
 import BN from "bn.js";
-import MnemonicKeyring from "mnemonic-keyring";
 
+const getImplicitAccountId = (publicKey: string) => {
+  return Buffer.from(decode(publicKey.replace('ed25519:', ''))).toString('hex');
+}
+
+export const nearNetworkId = "testnet";
 export let nearWallet: WalletConnection;
 export let nearAddresses: string[];
 
 /**
  * Utilities
  */
-export async function createOrRestoreNearWallet() {
+export async function createOrRestoreNearWallet(mnemonic: string) {
   const localStorageKeyStore = new keyStores.BrowserLocalStorageKeyStore();
+
+  const keyPair1 = parseSeedPhrase(mnemonic, "m/44'/397'/0'/0'/0'");
+  const account1 = getImplicitAccountId(keyPair1.publicKey);
+  await localStorageKeyStore.setKey(
+    nearNetworkId,
+    account1,
+    utils.KeyPair.fromString(keyPair1.secretKey)
+  );
+
+  const keyPair2 = parseSeedPhrase(mnemonic, "m/44'/397'/0'/0'/0'");
+  const account2 = getImplicitAccountId(keyPair2.publicKey);
+  await localStorageKeyStore.setKey(
+    nearNetworkId,
+    account2,
+    utils.KeyPair.fromString(keyPair2.secretKey)
+  );
+
   const near = await connect({
     keyStore: localStorageKeyStore,
-    networkId: "testnet",
+    networkId: nearNetworkId,
     nodeUrl: "https://rpc.testnet.near.org",
     walletUrl: "https://wallet.testnet.near.org",
     helperUrl: "https://helper.testnet.near.org",
@@ -20,7 +43,7 @@ export async function createOrRestoreNearWallet() {
   });
 
   nearWallet = new WalletConnection(near, "near_app");
-  nearAddresses = nearWallet.isSignedIn() ? [nearWallet.account().accountId] : [];
+  nearAddresses = [account1, account2];
 
   return {
     nearWallet,
