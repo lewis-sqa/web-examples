8b1e9840f9570ba8ea0499a84b121866709db025
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-03-15 11:46:20 +0000

    Removed NEAR mainnet chain.

--- wallets/react-wallet-v2/src/pages/index.tsx
+++ wallets/react-wallet-v2/src/pages/index.tsx
@@ -3,7 +3,7 @@ import AccountPicker from '@/components/AccountPicker'
 import PageHeader from '@/components/PageHeader'
 import { COSMOS_MAINNET_CHAINS } from '@/data/COSMOSData'
 import { EIP155_MAINNET_CHAINS, EIP155_TEST_CHAINS } from '@/data/EIP155Data'
-import { NEAR_MAINNET_CHAINS, NEAR_TEST_CHAINS } from '@/data/NEARData'
+import { NEAR_TEST_CHAINS } from '@/data/NEARData'
 import SettingsStore from '@/store/SettingsStore'
 import { Text } from '@nextui-org/react'
 import { Fragment } from 'react'
@@ -20,9 +20,6 @@ export default function HomePage() {
       <Text h4 css={{ marginBottom: '$5' }}>
         Mainnets
       </Text>
-      {Object.values(NEAR_MAINNET_CHAINS).map(({ name, logo, rgb }) => (
-        <AccountCard key={name} name={name} logo={logo} rgb={rgb} address={nearAddress} />
-      ))}
       {Object.values(EIP155_MAINNET_CHAINS).map(({ name, logo, rgb }) => (
         <AccountCard key={name} name={name} logo={logo} rgb={rgb} address={eip155Address} />
       ))}
